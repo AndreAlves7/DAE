59411e5b56af966dbe5a5b4653969c11b7e601f2
André Colaço Alves Filho <2212948@my.ipleiria.pt>
2024-01-13 17:45:33 +0000

    Added entities

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/PackageEntity.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/PackageEntity.java
@@ -3,6 +3,7 @@ package pt.ipleiria.estg.dei.ei.dae.backend.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 import pt.ipleiria.estg.dei.ei.dae.backend.enums.PackageMaterialType;
@@ -20,5 +21,7 @@ public class PackageEntity extends AbstractEntity{
     @Enumerated(EnumType.STRING)
     private PackageMaterialType packageMaterial;
 
-
+    @OneToOne
+    private ProductEntity productEntity;
+    
 }
---
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/entities/ShipmentEntity.java
@@ -0,0 +1,7 @@
+package pt.ipleiria.estg.dei.ei.dae.backend.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class ShipmentEntity extends AbstractEntity{
+}
