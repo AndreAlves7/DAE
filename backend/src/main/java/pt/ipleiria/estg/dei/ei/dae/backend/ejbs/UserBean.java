57f8945fe24bb6710c21c7c61ea79058e2482a53
André Colaço Alves Filho <2212948@my.ipleiria.pt>
2024-01-13 16:05:21 +0000

    bugfix

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/ejbs/UserBean.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/ejbs/UserBean.java
@@ -1,10 +1,11 @@
 package pt.ipleiria.estg.dei.ei.dae.backend.ejbs;
 
 import jakarta.ejb.Stateless;
+import pt.ipleiria.estg.dei.ei.dae.backend.entities.UserEntity;
 
 @Stateless
-public class UserBean extends AbstractBean<UserBean> {
-    public UserBean(Class<UserBean> entityClass) {
+public class UserBean extends AbstractBean<UserEntity> {
+    public UserBean(Class<UserEntity> entityClass) {
         super(entityClass);
     }
 }
