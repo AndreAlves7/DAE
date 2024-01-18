746925fd8345ba49bf012fef4ab156d164eaecfb
Cancela <2212509@my.ipleiria.pt>
2024-01-18 20:20:20 +0000

    frontend change

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,64 +1,20 @@
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import { io } from 'socket.io-client'
-import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
-import PrimeVue from 'primevue/config';
-import ConfirmationService from 'primevue/confirmationservice';
-
-
-// bootstrap-vue-next
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-
-import Toast from "vue-toastification"
-import "vue-toastification/dist/index.css"
-
-import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'
-
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import FieldErrorMessage from './components/global/FieldErrorMessage.vue'
-
 import App from './App.vue'
 import router from './router'
-
-
-const apiDomain = import.meta.env.VITE_API_DOMAIN
-// const wsConnection = import.meta.env.VITE_WS_CONNECTION
-// app.provide('socket', io(wsConnection))
-const wsConnection = import.meta.env.VITE_WS_CONNECTION
-
-
-axios.defaults.baseURL = apiDomain + '/api'
-axios.defaults.headers.common['Content-type'] = 'application/json'
+import axios from 'axios'
 
 const app = createApp(App)
 
-app
-  .provide('socket', io(wsConnection))
-  .provide('serverBaseUrl', apiDomain)
-  .use(Toast, {
-    position: "top-center",
-    timeout: 3000,
-    closeOnClick: true,
-    pauseOnFocusLoss: true,
-    pauseOnHover: true,
-    draggable: true,
-    draggablePercent: 0.6,
-    showCloseButtonOnHover: true,
-    hideProgressBar: true,
-    closeButton: "button",
-    icon: true,
-    rtl: false
-  })
-  .use(createPinia())
-  .use(router)
-  .use(BootstrapIconsPlugin)
-  .use(PrimeVue)
-  .use(ConfirmationService)
-  .component('FieldErrorMessage', FieldErrorMessage)
-  .mount('#app')
+const apiDomain = 'http://localhost:8080'
+
+axios.defaults.baseURL = apiDomain + '/backend/api'
+axios.defaults.headers.common['Content-type'] = 'application/json'
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,137 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useUserStore } from "../stores/user.js"
-
-import Login from "../components/Login.vue"
-import Dashboard from "../components/Dashboard.vue"
-import Signup from "../components/auth/Signup.vue"
-import Vcards from "../components/admin/Vcards.vue"
-import Vcard from "../components/admin/Vcard.vue"
-import Admins from "../components/admin/administrators/Admin.vue"
-import adminCreate from "../components/admin/administrators/adminCreate.vue"
-import AdminCategories from "../components/admin/administrators/AdminCategories.vue"
-import DefaultCategoryCreate from "../components/admin/administrators/DefaultCategoryCreate.vue"
-import CreditTransaction from "../components/admin/transactions/CreditTransactionCreate.vue"
-import Transactions from "../components/TransactionsTable.vue"
-import Transaction from "../components/Transaction.vue"
-import MakeTransaction from "../components/vcard/MakeTransaction.vue"
-import Categories from "../components/categories/Categories.vue"
-import Category from "../components/categories/Category.vue"
-import CategoryCreate from "../components/categories/CategoryCreate.vue"
-
-let handlingFirstRoute = true
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Dashboard
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: Login
-        },
-        {
-            path: '/signup',
-            name: 'Signup',
-            component: Signup
-        },
-        {
-            path: '/dashboard',
-            name: 'Dashboard',
-            component: Dashboard
-        },
-        {
-            path: '/maketransaction',
-            name: 'MakeTransaction',
-            component: MakeTransaction
-        },
-        {
-            path: '/admin/vcards',
-            name: 'Vcards',
-            component: Vcards
-        },
-        {
-            path: '/admin/vcards/:phone_number',
-            name: 'Vcard',
-            component: Vcard,
-            props: route => ({ phone_number: parseInt(route.params.phone_number) })
-        },
-        {
-            path: '/admin/admins',
-            name: 'Admins',
-            component: Admins
-        },
-        {
-            path: '/admin/add',
-            name: 'AddAdmin',
-            component: adminCreate
-        },
-        {
-            path: '/vcard/transactions',
-            name: 'Transactions',
-            component: Transactions
-        },  
-         {
-            path: '/admin/categories',
-            name: 'AdminCategories',
-            component: AdminCategories
-        },
-        {
-            path: '/admin/transactions/add',
-            name: 'CreditTransaction',
-            component: CreditTransaction
-        },
-        {
-            path: '/vcard/category',
-            name: 'Categories',
-            component: Categories
-        },
-        {
-            path: '/vcard/category/:id',
-            name: 'Category',
-            component: Category,
-            props: route => ({ id: parseInt(route.params.id)})
-        },
-        {
-            path: '/vcard/category/add',
-            name: 'AddCategory',
-            component: CategoryCreate
-        },   
-        {
-            path: '/admin/category/add',
-            name: 'DefaultCategoryCreate',
-            component: DefaultCategoryCreate
-        },
-        {
-            path: '/vcard/transactions/:id',
-            name: 'Transaction',
-            component: Transaction,
-        }
-    ]
-})
-
-router.beforeEach(async (to, from, next) => {
-    const userStore = useUserStore()
-    if (handlingFirstRoute) {
-      handlingFirstRoute = false
-      await userStore.restoreToken()
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
+    },
+    {
+      path: '/createproduct',
+      name: 'createProduct',
+      component: () => import('../views/supplier/CreateProductView.vue')
+    },
+    {
+      path: '/viewproducts',
+      name: 'ViewProducts',
+      component: () => import('../views/supplier/ViewProductsView.vue')
+    },
+    {
+      path: '/updateproduct/:id',
+      name: 'UpdateProduct',
+      component: () => import('../views/supplier/UpdateProductView.vue'),
     }
-    if ((to.name == 'Login') || (to.name == 'Signup')) {
-      next()
-      return
-    }
-    if (!userStore.user) {
-      next({ name: 'Login' })
-      return
-    }
-    if(userStore.userType == 'A' && (to.name == 'home')) {
-        next({name: 'Vcards'})
-        return
-    }
-    next()
+  ]
 })
 
 export default router
